ec4b68f803cd8d5d32410e48ed0c1a2eff6ce049
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-09-26 18:51:43 +0200

    docker-compose works!!!!!!!

--- server.js
+++ server.js
@@ -13,10 +13,14 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
+        connectionString: process.env.DB_URI,
+        /*ssl: {
             rejectUnauthorized: false
           }
+        host : process.env.DB_HOST,
+        user : process.env.DB_USER,
+        password : process.env.DB_PASSWORD,
+        database : process.env.DB_NAME*/
     }
 });
 
