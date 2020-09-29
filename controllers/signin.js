de7ad907e58bf8b27a4b834829d6bcc91a36a846
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-09-29 12:06:38 +0200

    finished the backend part of the sessions with the getAuthTokenId Function

--- controllers/signin.js
+++ controllers/signin.js
@@ -25,8 +25,14 @@ const handleSignin = (db, bcrypt, req, res) => {
         .catch(err => res.status(400).json('Wrong credentials!'))
 }
 
-const getAuthTockenId = () => {
-    console.log('Auth ok!');
+const getAuthTockenId = (req, res) => {
+    const { authorization } = req.headers;
+    return redisClient.get(authorization, (err, reply) => {
+        if(err || !reply) {
+            return res.status(400).json('Unauthorized');
+        }
+        return res.json({id: reply});
+    })
 }
 
 const signToken = (email) => {
@@ -56,7 +62,7 @@ const createSession = (user) => {
 const signinAuthentication = (db, bcrypt) => (req, res) => {
     const { authorization } = req.headers;
     return authorization ? 
-        getAuthTockenId() : 
+        getAuthTockenId(req, res) : 
         handleSignin(db, bcrypt, req, res)
             .then(data => {
                 return data.id && data.email ? createSession(data) : Promise.reject('data');
